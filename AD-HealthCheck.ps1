41275232c8dcfa3b15942d2adde11c0352546a17
403 - Forbidden <60777262+G0urmetD@users.noreply.github.com>
2024-04-02 15:46:40 +0200

    Update AD-HealthCheck.ps1
    Adding functions and change sequence of tests.

--- AD-HealthCheck.ps1
+++ AD-HealthCheck.ps1
@@ -5,36 +5,46 @@
     G0urmetD (403 - Forbidden)
 #>
 #Requires -RunAsAdministrator
+
+function Check-Replication {
+    # check the replication status of all the domain controllers
+    Repadmin /replsummary
+}
+
+function Check-Services {
+    # check several Active Directory services
+    $Services = 'DNS','ADWS','DFSR','DHCP','NetLogon','GPSVC','KDC','MPSSVC','NTDS'
+    ForEach ($Service in $Services) {
+        Get-Service $Service | Select-Object Name,Status,DisplayName
+    }
+}
+
+function Check-UnsecureLDAPBinds {
+    # detect unsecure LDAP binds
+    Get-WinEvent -FilterHashtable @{
+                    LogName = 'Security'
+                        ID = 2889
+                    }
+}
+
+function Check-DomainControllerAndDNS {
+    # test several aspects of a domain controller including DNS
+    DCDiag /Test:DNS /e /v
+}
+
+# main script
 Write-Host -ForegroundColor Blue "[*]" -NoNewline
 Write-Host " Domain Controller Health Check:"
-Write-Host -ForegroundColor Blue "[*]" -NoNewline
-Write-Host " Domain & Domain Controller Services:"
+Check-Replication
+Write-Output "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 Write-Host -ForegroundColor Blue "[*]" -NoNewline
 Write-Host " Domain Controller & DNS Test:"
+Check-DomainControllerAndDNS
+Write-Output "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 Write-Host -ForegroundColor Blue "[*]" -NoNewline
 Write-Host " Find unsecure LDAP binds:"
-Write-Output ""
-
+Check-UnsecureLDAPBinds
 Write-Output "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-Write-Output "[*] Checking Domain Controller Replication:"
-# check the replication status of all the domain controllers
-Repadmin /replsummary
-
-Write-Output "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-Write-Output "[*] Checking Domain and Domain Controller Services:"
-# check several Active Directory services
-$Services = 'DNS','ADWS','DFSR','DHCP','NetLogon','GPSVC','KDC','MPSSVC','NTDS'
-ForEach ($Service in $Services) {Get-Service $Service | Select-Object Name, Status,DisplayName}
-
-Write-Output "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-Write-Output "[*] Checking for unsecure LDAP binds:"
-# detect unsecure LDAP binds
-Get-WinEvent -FilterHashtable @{
-                LogName = 'Security'
-                    ID = 2889
-                }
-
-Write-Output "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-Write-Output "[*] Domain Controller test including DNS:"
-# test several aspects of a domain controller including DNS
-DCDiag /Test:DNS /e /v
+Write-Host -ForegroundColor Blue "[*]" -NoNewline
+Write-Host " Domain & Domain Controller Services:"
+Check-Services
